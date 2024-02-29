f3dbe4b2d72aa4258d0346e442ec967cb92cd49c
Mathias <eaamatoj@students.eaaa.dk>
2024-02-29 10:43:56 +0100

    Created method body in Loan>CalculateFine

--- examples/tests/opgave01/models/CarInsuranceTest.java
+++ examples/tests/opgave01/models/CarInsuranceTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class CarInsuranceTest {
 
     @Test
-    @DisplayName("Premiun for Male 18 years, 0 yearsExperience")
+    @DisplayName("Premium for Male 18 years, 0 yearsExperience")
     void calculatePremiumMale18Years0Experience() {
         CarInsurance carInsurance = new CarInsurance(1000);
         double actual = carInsurance.calculatePremium(18,false,0);
@@ -18,7 +18,7 @@ class CarInsuranceTest {
     }
 
     @Test
-    @DisplayName("Premiun for Male 25 years, 5 yearsExperience")
+    @DisplayName("Premium for Male 25 years, 5 yearsExperience")
     void calculatePremiumMale25Years5Experience() {
         CarInsurance carInsurance = new CarInsurance(1000);
         double actual = carInsurance.calculatePremium(25,false,5);
@@ -28,7 +28,7 @@ class CarInsuranceTest {
     }
 
     @Test
-    @DisplayName("Premiun for Female 30 years, 9 yearsExperience")
+    @DisplayName("Premium for Female 30 years, 9 yearsExperience")
     void calculatePremiumFemale30Years9Experience() {
         CarInsurance carInsurance = new CarInsurance(1000);
         double actual = carInsurance.calculatePremium(30,true,9);
@@ -36,4 +36,17 @@ class CarInsuranceTest {
 
         assertEquals(expected,actual,0.1);
     }
+
+    /**
+     * @Test
+     *     @DisplayName("ThrowsError")
+     *     void calculatePremiumThrowsError() {
+     *         CarInsurance carInsurance = new CarInsurance(1000);
+     *         double actual = carInsurance.calculatePremium(15,true,9);
+     *         double expected = 617.5;
+     *
+     *         assertEquals(expected,actual,0.1);
+     *     }
+     */
+
 }
--- src/opgave02/models/Loan.java
+++ src/opgave02/models/Loan.java
@@ -10,7 +10,22 @@ public class Loan {
     }
 
     public int calculateFine(LocalDate calculatedDate, LocalDate actualDate, boolean adult) {
-        return 0;
+        int fine = 0;
+        int days = (int) (actualDate.toEpochDay() - calculatedDate.toEpochDay());
+
+        if (days > 0 && days <= 7) {
+            fine += 10;
+        } else if (days <= 14) {
+            fine += 30;
+        } else if (days >= 15) {
+            fine += 45;
+        }
+
+        if (adult) {
+            return fine * 2;
+        } else {
+            return fine;
+        }
     }
 
 }
